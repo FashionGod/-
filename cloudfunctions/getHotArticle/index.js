0c39f129e408cc06b3d88b006b0f41f6456c066a
覃耀峻 <4559908@qq.com>
2020-07-25 01:18:59 +0800

    修改授权说明

--- cloudfunctions/getHotArticle/index.js
+++ cloudfunctions/getHotArticle/index.js
@@ -9,7 +9,7 @@ const _ = db.command
 const $ = db.command.aggregate
 
 // 云函数入口函数
-exports.main = async(event, context) => {
+exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
   return db.collection('article')
     .aggregate()
