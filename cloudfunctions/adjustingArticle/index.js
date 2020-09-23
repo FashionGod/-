9ecbc0c98e71468f2babea8c24b1b26a6f1abb87
覃耀峻 <4559908@qq.com>
2020-09-23 15:02:29 +0800

    重写校验文章信息

--- cloudfunctions/adjustingArticle/index.js
+++ cloudfunctions/adjustingArticle/index.js
@@ -5,142 +5,83 @@ cloud.init({
 })
 const db = cloud.database()
 const _ = db.command
+const $ = db.command.aggregate
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  let liked = false
-  let collected = false
-  let test = []
-  const tasks1 = []
-  const tasks2 = []
-  const tasks3 = []
-  const tasks4 = []
-  const tasks5 = []
-  const tasks6 = []
-  let allarticle_ids = []
-  let allArticleLikeCounts = []
-  let allArticleCommentCounts = []
-  let allArticleCollectCounts = []
-  await db.collection('article').field({
-    _id: true,
-  })
-    .get().then(res => {
-      test = res.data
-      res.data.forEach(v => {
-        // v.forEach(v1 => {
-        allarticle_ids.push(v._id)
-        // })
-      })
-      allarticle_ids.forEach(v => {
-        const promise1 = db.collection('like')
-          .where({
-            likeId: v
-          }).get()
-        promise1.then(res => {
-          // test = allarticle_ids
-          allArticleLikeCounts.push(res.data.length)
-          const promise3 = db.collection('article')
-            .where({
-              _id: v,
-            }).update({
-              data: {
-                likeCount: res.data.length,
-              },
-            })
-          tasks3.push(promise3)
-        })
-        tasks1.push(promise1)
-      })
+  checkLike()
+  checkComment()
+  checkCollect()
+}
 
-      allarticle_ids.forEach(v => {
-        const promise2 = db.collection('comment')
-          .where({
-            articleId: v
-          }).get()
-        promise2.then(res => {
-          // test = allarticle_ids
-          allArticleCommentCounts.push(res.data.length)
-          const promise4 = db.collection('article')
-            .where({
-              _id: v,
-            }).update({
-              data: {
-                commentCount: res.data.length,
-                // hotComment:{}只有在完全洗白情况下开启
-              },
-            })
-          tasks4.push(promise4)
-        })
-        tasks2.push(promise2)
-      })
-      allarticle_ids.forEach(v => {
-        const promise5 = db.collection('collect')
-          .where({
-            articleId: v
-          }).get()
-        promise5.then(res => {
-          // test = allarticle_ids
-          allArticleCollectCounts.push(res.data.length)
-          const promise6 = db.collection('article')
-            .where({
-              _id: v,
-            }).update({
-              data: {
-                collectionCount: res.data.length,
-                // hotComment:{}只有在完全洗白情况下开启
-              },
-            })
-          tasks6.push(promise6)
-        })
-        tasks5.push(promise5)
-      })
-
-      // const promise1 = db.collection('like')
-      // .where({
-      // likeId: _.in(allarticle_ids)
-      // // type: _.in(types)
-      // }).get()
-      // promise1.then(res => {
-      // // test = allarticle_ids
-      // allArticleLikeCounts = res
-      // })
-      // tasks1.push(promise1)
-      // await db.collection('like').where({
-      // articleId: _.in(allarticle_ids)
-      // }).get().then(res => {
-      // allArticleLikeCounts = res
-      // })
+async function checkLike() {
+  let tasks = []
+  let res = await db.collection('like').aggregate()
+    .group({
+      _id: '$articleId',
+      num: $.sum(1)
     })
-  // let c = await db.collection('like').where({
-  // type: 1,
-  // openId: wxContext.OPENID,
-  // likeId: event.articleId
-  // }).count()
-  // c = c.total
-  // if (c > 0) {
-  // liked = true
-  // }
-  // c = await db.collection('collect').where({
-  // type: 1,
-  // openId: wxContext.OPENID,
-  // collectId: event.articleId
-  // }).count()
-  // c = c.total
-  // if (c > 0) {
-  // collected = true
-  // }
-  await Promise.all(tasks1)
-  await Promise.all(tasks2)
-  await Promise.all(tasks3)
-  await Promise.all(tasks4)
-  await Promise.all(tasks5)
-  await Promise.all(tasks6)
-  return {
-    success: true,
-    test: test,
-    allarticle_ids: allarticle_ids,
-    allArticleLikeCounts: allArticleLikeCounts,
-    allArticleCommentCounts: allArticleCommentCounts,
-    allArticleCollectCounts: allArticleCollectCounts,
-  }
+    .limit(9999)
+    .end()
+  res.list.forEach(v => {
+    console.log(v._id, v.num)
+    tasks.push(db.collection('article')
+      .where({
+        _id: v._id,
+      }).update({
+        data: {
+          likeCount: v.num,
+        },
+      }))
+  })
+  res = await Promise.all(tasks)
+  console.log('like', res)
+}
+
+async function checkComment() {
+  let tasks = []
+  let res = await db.collection('comment').aggregate()
+    .group({
+      _id: '$articleId',
+      num: $.sum(1)
+    })
+    .limit(9999)
+    .end()
+  res.list.forEach(v => {
+    console.log(v._id, v.num)
+    tasks.push(db.collection('article')
+      .where({
+        _id: v._id,
+      }).update({
+        data: {
+          commentCount: v.num,
+        },
+      }))
+  })
+  res = await Promise.all(tasks)
+  console.log('comment', res)
+}
+
+async function checkCollect() {
+  let tasks = []
+  let res = await db.collection('collect').aggregate()
+    .group({
+      _id: '$articleId',
+      num: $.sum(1)
+    })
+    .limit(9999)
+    .end()
+  res.list.forEach(v => {
+    console.log(v._id, v.num)
+    tasks.push(db.collection('article')
+      .where({
+        _id: v._id,
+      }).update({
+        data: {
+          collectionCount: v.num,
+        },
+      }))
+  })
+  res = await Promise.all(tasks)
+  console.log('collect', res)
 }
